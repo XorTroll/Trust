4aee5b6f4fd54e7413c7d2d493542cd1f7823bbb
XorTroll <xabofdez@outlook.es>
2019-03-15 18:25:24 +0100

    Add first SVC tests

--- src/main.rs
+++ src/main.rs
@@ -2,18 +2,14 @@
 #![allow(non_snake_case)]
 #![feature(asm)]
 
-pub mod svc
-{
-    pub fn sleepThread(nano: i64)
-    {
-        unsafe
-        {
-            asm!("svc 0x0B" : : "{x0}"(nano) : "x0", "x1", "x2", "x3", "x4", "x5", "x6", "x7", "x8", "x9", "x10", "x11", "x12", "x13", "x14", "x15", "x16", "x17", "x18" : "volatile");
-        }
-    }
-}
+mod svc;
 
 fn main()
 {
-    svc::sleepThread(1000000000);
+    // This SVC can be used on emulators as console output, as in actual consoles we wouldn't have a better way to print stuff
+    svc::outputDebugString(String::from("Test output!"));
+    loop
+    {
+        // I finish with this infinite loop as the custom std impl for Rust is outdated and keeps logging invalid handle shit on emulators
+    }
 }
